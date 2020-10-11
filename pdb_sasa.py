3cc1c699ff472f80e8935ba8c8d7fa7736ea33fe
Mahyar Irani <mahiarirani@yahoo.com>
2020-10-11 20:48:00 +0330

    create get_coordinates function

--- pdb_sasa.py
+++ pdb_sasa.py
@@ -16,12 +16,19 @@ def probe(n=100):
 
     return points
 
+
+def get_coordinates(atom):
+    return [atom.get_coord()[0], atom.get_coord()[1], atom.get_coord()[2]]
+
+
+coords = []
 radius = 1.4
 parser = PDBParser()
 structure = parser.get_structure("2RSC", "2RSC.pdb")
 atoms = numpy.array(Selection.unfold_entities(structure, "A"))
 for atom in atoms:
+    coords.append(get_coordinates(atom))
+    atom.accessibility = 0
     atom.probe = probe()
     atom.probe += atom.get_coord()
     atom.probe *= radius
-
