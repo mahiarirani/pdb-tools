60f17f738a0fe0f513fe4cee39489a7134cba4a2
Mahyar Irani <mahiarirani@yahoo.com>
2020-10-11 20:42:45 +0330

    create probe function

--- pdb_sasa.py
+++ pdb_sasa.py
@@ -1,5 +1,20 @@
 from Bio.PDB import *
 
+def probe(n=100):
+    points = []
+    phi = math.pi * (3. - math.sqrt(5.))
+
+    for i in range(n):
+        y = 1 - (i / float(n - 1)) * 2
+        radius = math.sqrt(1 - y * y)
+        theta = phi * i
+        x = math.cos(theta) * radius
+        z = math.sin(theta) * radius
+
+        points.append((x, y, z))
+
+    return points
+
 parser = PDBParser()
 structure = parser.get_structure("2RSC", "2RSC.pdb")
 residues = []
