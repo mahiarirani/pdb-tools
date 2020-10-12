94ed2dff03ee5337194d7aca47c5ef6501d9add2
Mahyar Irani <mahiarirani@yahoo.com>
2020-10-12 13:26:10 +0330

    add atom radii table file refactor probe radius and points variable

--- pdb_sasa.py
+++ pdb_sasa.py
@@ -17,7 +17,7 @@ from sklearn.neighbors import KDTree
 # - add timer
 # - convert percentage to area measurement unit
 
-def load_atom_radii(file):
+def load_atom_radii(file='atom_radii.csv'):
     atom_radii_dict = {}
     with open(file, 'r') as data:
         for line in csv.DictReader(data):
@@ -39,10 +39,10 @@ def get_coordinates(atom):
     return [atom.get_coord()[0], atom.get_coord()[1], atom.get_coord()[2]]
 
 
-atom_radii = load_atom_radii('atom_radii.csv')
+atom_radii = load_atom_radii()
 coords = []
-radius = 1.4
-probe_points = 10
+probe.radius = 1.4
+probe.points = 10
 parser = PDBParser()
 structure = parser.get_structure("2RSC", "2RSC.pdb")
 atoms = np.array(Selection.unfold_entities(structure, "A"))
@@ -50,14 +50,14 @@ for atom in atoms:
     coords.append(get_coordinates(atom))
     atom.accessibility = 0
     atom.radius = atom_radii[atom.element]
-    atom.probe = probe(probe_points, atom.get_coord(), radius + atom.radius)
+    atom.probe = probe(probe.points, atom.get_coord(), probe.radius + atom.radius)
 
 coords = np.array(coords)
 tree = KDTree(coords)
 
 for index, atom in enumerate(atoms):
     atom.accessibility = sum(
-        sum([i != 0 for i in [tree.query_radius(atom.probe, atom.radius + radius, count_only=True)]])) / probe_points
+        sum([i != 0 for i in [tree.query_radius(atom.probe, atom.radius + probe.radius, count_only=True)]])) / probe_points
     print('Atom #%s [%s] SASA is %s' % (index+1, atom.element, atom.accessibility))
 
 total = sum(atom.accessibility for atom in atoms)
