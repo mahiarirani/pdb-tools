ed929d5df3edab2001c01215fd5a6674edf91a85
Mahyar Irani <mahiarirani@yahoo.com>
2020-10-13 00:18:23 +0330

    refactor code into class display output by type input arguments input PDB file convert result to Angstroms update TODO

--- pdb_sasa.py
+++ pdb_sasa.py
@@ -1,3 +1,6 @@
+import tkinter as tk
+from tkinter import filedialog
+import os
 import csv
 import numpy as np
 from Bio.PDB import *
@@ -6,60 +9,114 @@ from sklearn.neighbors import KDTree
 
 #TODO
 # + include atoms size
-# - output by type
 # + find right neighbours
-# - input pdb file
-# - visualize atom
-# - classify the program
 # + optimize
 # + change probe
-# - input arguments
+# + input pdb file
+# + classify the program
+# + input arguments
+# + output by type
+# + convert percentage to area measurement unit
 # - add timer
-# - convert percentage to area measurement unit
+# - visualize atom
+# - calculate polar/apolar surface
+# - concurrent processing
 
-def load_atom_radii(file='atom_radii.csv'):
-    atom_radii_dict = {}
-    with open(file, 'r') as data:
-        for line in csv.DictReader(data):
-            atom_radii_dict[line['atom']] = int(line['radii']) / 100
-    return atom_radii_dict
+class PDB:
+    def __init__(self, probe_points=100, probe_radius=1.4):
+        self.probe_points = probe_points
+        self.probe_radius = probe_radius
+
+        self.atom_radii = self.load_atom_radii()
+
+        self.structure = self.load_pdb("2rsc.pdb")
+        self.atoms = self.get_atoms()
+        self.coords = []
+        self.coords = self.get_all_coordinates()
+
+    @staticmethod
+    def load_pdb(file=None):
+        if file is None:
+            root = tk.Tk()
+            root.withdraw()
+            file = filedialog.askopenfilename()
+
+        path, file = os.path.split(file)
+        file_name, file_extension = os.path.splitext(file)
+
+        parser = PDBParser()
+        return parser.get_structure(file_name, path + file)
+
+    @staticmethod
+    def load_atom_radii(file='atom_radii.csv'):
+        atom_radii_dict = {}
+        with open(file, 'r') as data:
+            for line in csv.DictReader(data):
+                atom_radii_dict[line['atom']] = int(line['radii']) / 100
+        return atom_radii_dict
+
+    def get_atoms(self):
+        return Selection.unfold_entities(self.structure, "A")
+
+    @staticmethod
+    def get_coordinates(a):
+        return [a.get_coord()[0], a.get_coord()[1], a.get_coord()[2]]
+
+    def get_all_coordinates(self):
+        for atom in self.atoms:
+            self.coords.append(self.get_coordinates(atom))
+        return np.array(self.coords)
+
+    @staticmethod
+    def create_probe(n, atom_coords, atom_radius):
+        indices = np.arange(0, n, dtype=float) + 0.5
+
+        phi = np.arccos(1 - 2 * indices / n)
+        theta = np.pi * (1 + 5 ** 0.5) * indices
+
+        points = np.dstack([np.cos(theta) * np.sin(phi), np.sin(theta) * np.sin(phi), np.cos(phi)])
+        return (points[0] + atom_coords) * atom_radius
+
+    def attach_probe(self):
+        print('----------\nBegin Probe Attachment')
+        for atom in self.atoms:
+            atom.radius = self.atom_radii[atom.element]
+            atom.probe = self.create_probe(self.probe_points, atom.get_coord(), self.probe_radius + atom.radius)
+        print('Probe Attached Successfully\n----------')
+
+    def sasa(self):
+        print('----------\nBegin SASA Calculation')
+        tree = KDTree(self.coords)
+        for index, atom in enumerate(self.atoms):
+            radius = (self.probe_radius + atom.radius)
+            atom.accessibility = round((4 * np.pi * radius ** 2) * (sum(sum(
+                [i != 0 for i in [tree.query_radius(atom.probe, radius, count_only=True)]])) / self.probe_points), 2)
+            print('Atom #%s [%s] SASA is %s Å' % (index + 1, atom.element, atom.accessibility))
+        print('SASA Calculated Successfully\n----------')
+
+    @staticmethod
+    def calc_sasa(item):
+        atoms = np.array(Selection.unfold_entities(item, "A"))
+        return round(sum(atom.accessibility for atom in atoms), 2)
+
+    def output(self, method=''):
+        print('----------\nResult\n----------')
+        for model in self.structure:
+            if method.__contains__('m'):
+                print('Model #%s SASA is %s Å' % (model.id, self.calc_sasa(model)))
+            for chain in model:
+                if method.__contains__('c'):
+                    print('Chain #%s SASA is %s Å' % (chain.id, self.calc_sasa(chain)))
+                for residue in chain:
+                    if method.__contains__('r'):
+                        print('Residue #%s SASA is %s Å' % (residue.get_resname(), self.calc_sasa(residue)))
+                    for atom in residue:
+                        if method.__contains__('a'):
+                            print('Atom #%s SASA is %s Å' % (atom.get_name(), atom.accessibility))
+        print('Total SASA of %s is %s Å' % (self.structure.get_id(), self.calc_sasa(self.structure)))
 
 
-def probe(n, coords, radius):
-    indices = np.arange(0, n, dtype=float) + 0.5
-
-    phi = np.arccos(1 - 2 * indices / n)
-    theta = np.pi * (1 + 5 ** 0.5) * indices
-
-    points = np.dstack([np.cos(theta) * np.sin(phi), np.sin(theta) * np.sin(phi), np.cos(phi)])
-    return (points[0] + coords) * radius
-
-
-def get_coordinates(atom):
-    return [atom.get_coord()[0], atom.get_coord()[1], atom.get_coord()[2]]
-
-
-atom_radii = load_atom_radii()
-coords = []
-probe.radius = 1.4
-probe.points = 10
-parser = PDBParser()
-structure = parser.get_structure("2RSC", "2RSC.pdb")
-atoms = np.array(Selection.unfold_entities(structure, "A"))
-for atom in atoms:
-    coords.append(get_coordinates(atom))
-    atom.accessibility = 0
-    atom.radius = atom_radii[atom.element]
-    atom.probe = probe(probe.points, atom.get_coord(), probe.radius + atom.radius)
-
-coords = np.array(coords)
-tree = KDTree(coords)
-
-for index, atom in enumerate(atoms):
-    atom.accessibility = sum(
-        sum([i != 0 for i in
-             [tree.query_radius(atom.probe, atom.radius + probe.radius, count_only=True)]])) / probe.points
-    print('Atom #%s [%s] SASA is %s' % (index + 1, atom.element, atom.accessibility))
-
-total = sum(atom.accessibility for atom in atoms)
-print('Total SASA of %s is %s percent' % (structure.get_id(), round(total / atoms.size * 100, 2)))
+myPDB = PDB()
+myPDB.attach_probe()
+myPDB.sasa()
+myPDB.output('mcra')
