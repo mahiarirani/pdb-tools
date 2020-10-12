e4eaafc524722628fdb874dded783dc996acde17
Mahyar Irani <mahiarirani@yahoo.com>
2020-10-12 13:16:37 +0330

    include atom element radius

--- pdb_sasa.py
+++ pdb_sasa.py
@@ -1,20 +1,29 @@
+import csv
 import numpy as np
 from Bio.PDB import *
 from sklearn.neighbors import KDTree
 
 
 #TODO
-# - include atoms size
+# + include atoms size
 # - output by type
-# - find right neighbours
+# + find right neighbours
 # - input pdb file
 # - visualize atom
 # - classify the program
-# - optimize
+# + optimize
 # + change probe
-# - input probe radius
+# - input arguments
 # - add timer
 # - convert percentage to area measurement unit
+
+def load_atom_radii(file):
+    atom_radii_dict = {}
+    with open(file, 'r') as data:
+        for line in csv.DictReader(data):
+            atom_radii_dict[line['atom']] = int(line['radii']) / 100
+    return atom_radii_dict
+
 
 def probe(n, coords, radius):
     indices = np.arange(0, n, dtype=float) + 0.5
@@ -30,6 +39,7 @@ def get_coordinates(atom):
     return [atom.get_coord()[0], atom.get_coord()[1], atom.get_coord()[2]]
 
 
+atom_radii = load_atom_radii('atom_radii.csv')
 coords = []
 radius = 1.4
 probe_points = 10
@@ -39,15 +49,16 @@ atoms = np.array(Selection.unfold_entities(structure, "A"))
 for atom in atoms:
     coords.append(get_coordinates(atom))
     atom.accessibility = 0
-    atom.probe = probe(probe_points, atom.get_coord(), radius)
+    atom.radius = atom_radii[atom.element]
+    atom.probe = probe(probe_points, atom.get_coord(), radius + atom.radius)
 
 coords = np.array(coords)
 tree = KDTree(coords)
 
 for index, atom in enumerate(atoms):
     atom.accessibility = sum(
-        sum([i != 0 for i in [tree.query_radius(atom.probe, radius, count_only=True)]])) / probe_points
-    print('Atom #%s SASA is %s' % (index, atom.accessibility))
+        sum([i != 0 for i in [tree.query_radius(atom.probe, atom.radius + radius, count_only=True)]])) / probe_points
+    print('Atom #%s [%s] SASA is %s' % (index+1, atom.element, atom.accessibility))
 
 total = 0
 for atom in atoms:
