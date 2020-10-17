ecdd3730866edbc14fd82afd851acc7301b29bed
Mahyar Irani <mahiarirani@yahoo.com>
2020-10-17 23:07:45 +0330

    new approach for sasa calculation (more accurate, but slower)

--- pdb_sasa.py
+++ pdb_sasa.py
@@ -31,8 +31,9 @@ class PDB:
 
         self.structure = self.load_pdb()
         self.atoms = self.get_atoms()
-        self.coords = []
+        self.attach_probe()
         self.coords = self.get_all_coordinates()
+        self.atom_tree, self.probe_tree = self.create_tree(self.coords)
 
     @staticmethod
     def load_pdb(file=None):
@@ -63,9 +64,22 @@ class PDB:
         return [a.get_coord()[0], a.get_coord()[1], a.get_coord()[2]]
 
     def get_all_coordinates(self):
+        atom_coords = []
+        probe_coords = []
         for atom in self.atoms:
-            self.coords.append(self.get_coordinates(atom))
-        return np.array(self.coords)
+            atom_coords.append(self.get_coordinates(atom))
+            for probe in atom.probe:
+                probe_coords.append(probe)
+        return {'atom': np.array(atom_coords), 'probe': np.array(probe_coords)}
+
+    @staticmethod
+    def create_tree(items):
+        trees = []
+        print('----------\nBegin Tree Creation')
+        for item in items:
+            trees.append(KDTree(items[item]))
+        print('Tree Created Successfully\n----------')
+        return trees
 
     @staticmethod
     def create_probe(n, atom_coords, atom_radius):
@@ -86,20 +100,18 @@ class PDB:
 
     def sasa(self):
         print('----------\nBegin SASA Calculation')
-        tree = KDTree(self.coords)
-        radius = (self.probe_radius + self.atom_radii[max(self.atom_radii, key=lambda i: self.atom_radii[i])])
         for index, atom in enumerate(self.atoms):
-            nna, probes = tree.query_radius(atom.probe, radius, return_distance=True)
-            atom.accessibility = 0
-            for idx, probe in enumerate(probes):
-                if probe.size > 0:
-                    probe = np.sort(probe)
-                    neighbour = round(probe[0], 2)
-                    if neighbour < atom.radius + self.probe_radius:
-                        atom.accessibility += 1
-            atom.accessibility = 1 - atom.accessibility / self.probe_points
+            radius = (self.probe_radius + atom.radius)
+            points, dist = self.probe_tree.query_radius([self.get_coordinates(atom)], radius, return_distance=True)
+            for point in points[0]:
+                self.coords['probe'][point] = 0
+            print('Atom #%s [%s] SASA is calculating' % (index + 1, atom.element))
+        for index, atom in enumerate(self.atoms):
+            pp = self.probe_points
+            atom_probe_points = self.coords['probe'][index * pp:index * pp + pp]
+            atom.accessibility = sum([p != 0 for p in atom_probe_points])[0]
+            atom.accessibility /= self.probe_points
             atom.accessibility *= 4 * np.pi * (atom.radius + self.probe_radius) ** 2
-            print('Atom #%s [%s] SASA is %s Å' % (index + 1, atom.element, atom.accessibility))
         print('SASA Calculated Successfully\n----------')
 
     @staticmethod
@@ -125,6 +137,5 @@ class PDB:
 
 
 myPDB = PDB()
-myPDB.attach_probe()
 myPDB.sasa()
 myPDB.output()
