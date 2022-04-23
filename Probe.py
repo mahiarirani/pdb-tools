e0f75c250ce56dd3c07c7d5322571164caa85a28
Mahyar Irani <mahiarirani@protonmail.com>
2022-04-23 11:23:08 +0300

    add get points in atom probe by distance add atoms in atom probe add atoms points from neighbors atoms probe update get points in atom probe

--- Probe.py
+++ Probe.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.neighbors import KDTree
+from collections import OrderedDict
 from Timer import Timer
 from copy import deepcopy
 
@@ -57,10 +58,63 @@ class Probe:
         print('KDTree Created Successfully')
         return tree
 
-    def get_points_in_atom_probe(self, atoms):
-        radius = [(self.radius + atom.radius) - 0.001 for atom in atoms]
+    def get_points_in_atom_probe(self, atoms, include_probe_radius=True):
+        if include_probe_radius:
+            radius = [(self.radius + atom.radius) - 0.001 for atom in atoms]
+        else:
+            radius = [atom.radius for atom in atoms]
         atoms = [a.get_coord() for a in atoms]
         return self.tree.query_radius(atoms, radius)
+
+    def get_points_in_atom_probe_by_distance(self, atoms):
+        radius = [(self.radius + atom.radius) - 0.001 for atom in atoms]
+        atoms_coords = [a.get_coord() for a in atoms]
+        atoms_points = self.tree.query_radius(atoms_coords, radius, return_distance=True, sort_results=True)
+        probes_points = {}
+        for index, points in enumerate(atoms_points[0]):
+            for idx, point in enumerate(points):
+                if probes_points.get(point) is None:
+                    probes_points[point] = []
+                probes_points[point].append({'atom': index, 'distance': atoms_points[1][index][idx]})
+        for item in dict(filter(lambda x: len(x[1]) > 1, probes_points.items())):
+            probes_points[item].sort(key=lambda x: x['distance'])
+        return atoms_points
+
+    def get_atoms_in_atom_probe(self, atoms, residue=None, atoms_points=None):
+        if atoms_points is None:
+            atoms_points = self.get_points_in_atom_probe(atoms)
+        for index, points in enumerate(atoms_points):
+            neighbors_points = list(np.array(points) // self.points)
+            if residue is None:
+                neighbors = [{'atom': self.atoms[atom], 'share': neighbors_points.count(atom)}
+                             for atom in list(OrderedDict.fromkeys(neighbors_points))]
+            else:
+                neighbors = [{'atom': self.atoms[atom], 'share': neighbors_points.count(atom)}
+                             for atom in list(OrderedDict.fromkeys(neighbors_points))
+                             if self.atoms[atom].get_parent() != residue]
+            atoms[index].neighbors = neighbors
+        return atoms
+
+    def get_atoms_points_from_neighbors_atoms_probe(self, atoms, residue):
+        probes_points = [[[] for _ in range(self.points)] for _ in atoms]
+        atom_points = [self.get_points_in_atom_probe([a['atom'] for a in atom.neighbors], False) for atom in atoms]
+        for index, self_atom in enumerate(atom_points):
+            for idx, neighbor_atom_points in enumerate(self_atom):
+                for i, point in enumerate(neighbor_atom_points):
+                    if self.atoms[point // self.points].get_parent() == residue:
+                        atom_id = [atom.serial_number for atom in atoms].index(point // self.points + 1)
+                        probes_points[atom_id][point % self.points].append(atoms[index].neighbors[idx]['atom'])
+        probes_points = [list(f) for f in list(filter(lambda x: len(x) != 0, item) for item in probes_points)]
+        for index, self_atom in enumerate(probes_points):
+            atoms[index].neighbors = {}
+            ratio = (4 * np.pi * (atoms[index].radius + self.radius) ** 2) / self.points
+            for atom_neighbors in self_atom:
+                for atom in OrderedDict.fromkeys(atom_neighbors):
+                    if atoms[index].neighbors.get(atom) is None:
+                        atoms[index].neighbors[atom] = 0
+                    atoms[index].neighbors[atom] += atom_neighbors.count(atom) / len(atom_neighbors) * ratio
+            atoms[index].neighbors = [{'atom': atom, 'share': share} for atom, share in atoms[index].neighbors.items()]
+        return atoms
 
 
 class ProbeItem:
@@ -68,4 +122,3 @@ class ProbeItem:
         self.coords = coords
         self.buried = deepcopy(buried_list)
         self.atom = atom
-
